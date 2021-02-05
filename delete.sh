9c4928567dc2a9539fc571d6267fc18f16a6c54d
Narek Baghdasaryan <narek2011n@gmail.com>
2021-02-05 18:01:49 +0400

    Added postgres deployment

--- delete.sh
+++ delete.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-minikube kubectl -- delete -f pgadmin4/
 minikube kubectl -- delete -f nginx/
+minikube kubectl -- delete -f pgadmin4/
+minikube kubectl -- delete -f postgres/
--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+minikube kubectl -- apply -f nginx/
 minikube kubectl -- apply -f pgadmin4/
 echo "
 pgadmin4 is working! Credentials:
 email    - user@domain.com
 password - SuperSecret
 "
-minikube kubectl -- apply -f nginx/
+minikube kubectl -- apply -f postgres/
+echo "
+postgres is working! Credentials:
+db       = testdb
+user     = testuser
+password = TestPassWord123
+"
